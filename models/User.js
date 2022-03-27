a725c1750cd4be412a79f49d186c555875536013
hrendonc <hrchilpo@gmail.com>
2022-03-27 16:51:26 -0600

    Validación de usuario

--- models/User.js
+++ models/User.js
@@ -1,10 +1,27 @@
 const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
-    date: Date
+    name: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
+    },
+    email: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 1024
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
 })
 
 module.exports = mongoose.model('User', userSchema)
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,32 @@
 const router = require('express').Router()
 const User = require('../models/User')
+const Joi = require('@hapi/joi')
 
-router.post('/register', async (req, res)=>{
-    console.log(req.body.name)
+const schemaRegister = Joi.object({
+    name: Joi.string().min(6).max(255).required(),
+    email: Joi.string().min(6).max(355).required().email(),
+    password: Joi.string().min(6).max(1024).required()
+})
+
+router.post('/register', async (req, res)=>{   
     
+    // Validar usuario
+    const {error} = schemaRegister.validate(req.body)
+
+    if (error) {
+        return res.status(400).json({
+            error:'Ups! ' + error.message
+        })
+    }
+
+    // Validar si el correo ya esta registrado
+    const emailExist = await User.findOne({email: req.body.email})
+    if (emailExist) {
+        return res.status(400).json({
+            error: 'Correo ya registrado'
+        })
+    }
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
